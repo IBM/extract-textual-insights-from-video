f542e37ef322a4cca750bcf3a0e41aae1e754c2d
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2021-06-03 14:13:43 +0530

    Update deleteservice.sh

--- deleteservice.sh
+++ deleteservice.sh
@@ -2,16 +2,19 @@ servicename="cp-stt"
 ibmcloud resource service-key-delete "$servicename-creds" -f
 ibmcloud resource service-instance-delete $servicename -f
 rm speechtotext.json
+rm cp-stt.txt
 touch speechtotext.json
-
-servicename="cp-ta"
-ibmcloud resource service-key-delete "$servicename-creds" -f
-ibmcloud resource service-instance-delete $servicename -f
-rm naturallanguageunderstanding.json
-touch naturallanguageunderstanding.json
 
 servicename="cp-nlu"
 ibmcloud resource service-key-delete "$servicename-creds" -f
 ibmcloud resource service-instance-delete $servicename -f
+rm naturallanguageunderstanding.json
+rm cp-nlu.txt
+touch naturallanguageunderstanding.json
+
+servicename="cp-ta"
+ibmcloud resource service-key-delete "$servicename-creds" -f
+ibmcloud resource service-instance-delete $servicename -f
 rm toneanalyzer.json
+rm cp-ta.txt
 touch toneanalyzer.json
