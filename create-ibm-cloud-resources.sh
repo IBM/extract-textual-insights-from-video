3893fe7ea08abee196e4d64bec42c0540d51a506
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2021-09-15 13:33:14 +0530

    Update create-ibm-cloud-resources.sh

--- create-ibm-cloud-resources.sh
+++ create-ibm-cloud-resources.sh
@@ -48,14 +48,14 @@ region="eu-gb"
 
 cpserviceexists=$(ibmcloud resource service-instance $servicename 2>&1 | awk 'FNR == 2 {print}')
 if [ "$cpserviceexists" = "OK" ]; then
-    echo "(3/3) Text analytics service already exists in your IBM Cloud account, using the same resource"
+    echo "(3/3) Tone Analyzer service already exists in your IBM Cloud account, using the same resource"
     ibmcloud resource service-key "$servicename-creds" > "$servicename.txt"  2>&1
     apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
     url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
     JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
     echo $JSON_STRING > toneanalyzer.json
 elif [ "$cpserviceexists" = "FAILED" ]; then
-    echo "(3/3) Creating Text Analysis service"
+    echo "(3/3) Creating Tone Analyzer service"
     ibmcloud resource service-instance-create $servicename $service lite $region
     ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
     apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
