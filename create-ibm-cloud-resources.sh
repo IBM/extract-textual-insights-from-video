52feec433d549c74dc243f1961e95eda5f98e33a
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2021-09-15 13:28:35 +0530

    Update create-ibm-cloud-resources.sh

--- create-ibm-cloud-resources.sh
+++ create-ibm-cloud-resources.sh
@@ -2,7 +2,7 @@ servicename="cp-stt"
 service="speech-to-text"
 region="eu-gb"
 
-cpserviceexists=$(ibmcloud resource service-instance $servicename | awk 'FNR == 2 {print}')
+cpserviceexists=$(ibmcloud resource service-instance $servicename  2>&1 | awk 'FNR == 2 {print}')
 if [ "$cpserviceexists" = "OK" ]; then
     echo "(1/3) Speech to Text service already exists in your IBM Cloud account, using the same resource"
     ibmcloud resource service-key "$servicename-creds" > "$servicename.txt"  2>&1
@@ -24,7 +24,7 @@ servicename="cp-nlu"
 service="natural-language-understanding"
 region="eu-gb"
 
-cpserviceexists=$(ibmcloud resource service-instance $servicename | awk 'FNR == 2 {print}')
+cpserviceexists=$(ibmcloud resource service-instance $servicename 2>&1 | awk 'FNR == 2 {print}') 
 if [ "$cpserviceexists" = "OK" ]; then
     echo "(2/3) Natural language understanding service already exists in your IBM Cloud account, using the same resource"
     ibmcloud resource service-key "$servicename-creds" > "$servicename.txt"  2>&1
@@ -34,7 +34,7 @@ if [ "$cpserviceexists" = "OK" ]; then
     echo $JSON_STRING > naturallanguageunderstanding.json
 elif [ "$cpserviceexists" = "FAILED" ]; then
     echo "(2/3) Creating Natural Language Understanding service"
-    ibmcloud resource service-instance-create $servicename $service lite $region
+    ibmcloud resource service-instance-create $servicename $service free $region
     ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
     apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
     url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
@@ -46,7 +46,7 @@ servicename="cp-ta"
 service="tone-analyzer"
 region="eu-gb"
 
-cpserviceexists=$(ibmcloud resource service-instance $servicename | awk 'FNR == 2 {print}')
+cpserviceexists=$(ibmcloud resource service-instance $servicename 2>&1 | awk 'FNR == 2 {print}')
 if [ "$cpserviceexists" = "OK" ]; then
     echo "(3/3) Text analytics service already exists in your IBM Cloud account, using the same resource"
     ibmcloud resource service-key "$servicename-creds" > "$servicename.txt"  2>&1
