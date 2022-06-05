4512d3fceb42d5f7fd4cfacae09c8d5382827a75
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2022-06-05 12:41:18 +0530

    Update build.sh

--- container-image-builder/build.sh
+++ container-image-builder/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
-cd ../asset
-echo Checking for docker file
+cd ../
+echo "Checking for docker file"
     if [ -f "Dockerfile" ]; then
         echo "Dockerfile exists."
         echo "Building docker image"
-        read i < ../num.txt
+        read i < ./container-image-builder/num.txt
         i=`expr $i + 1`
-        echo $i > ../num.txt
-        Ver_Num=`cat ../num.txt`
+        echo $i > ./container-image-builder/num.txt
+        Ver_Num=`cat ./container-image-builder/num.txt`
         docker -H localhost:2375 build -t test:v$Ver_Num .
         echo "Docker Image Built"
         echo "Running docker image"
-        source ../run.sh
+        source ./container-image-builder/run.sh
     else 
         echo "Dockerfile does not exist."
     fi
